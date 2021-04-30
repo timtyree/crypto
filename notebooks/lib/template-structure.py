cc4e5fff15d02402edc4d157c4a74fcb1e2ae834
timtyree <tjgtyree@aol.com>
2021-04-30 15:50:12 -0700

    added template setups

---
+++ notebooks/lib/template-structure.py
@@ -0,0 +1,15 @@
+.
+└── src/
+    ├── main.py
+    ├── PackageA/
+    │   ├── __init__.py
+    │   ├── logic.py
+    │   ├── SubPackageA1/
+    │   │   ├── __init__.py
+    │   │   └── util.py
+    │   └── SubPackageA2/
+    │       ├── __init__.py
+    │       └── otherUtil.py
+    └── PackageB/
+        ├── __init__.py
+        └── helpers.py
